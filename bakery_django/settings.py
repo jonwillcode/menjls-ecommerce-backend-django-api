9d5a7627f618564a32fc002e04751714f95d78d8
Jonathan Williams <19499981+jonwillcode@users.noreply.github.com>
2021-11-01 17:18:00 -0400

    Fix S3 config

--- bakery_django/settings.py
+++ bakery_django/settings.py
@@ -169,7 +169,7 @@ if USE_S3:
     # s3 static settings
     AWS_LOCATION = 'static'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-    STATICFILES_STORAGE = 'bakery_django.storage_backends.StaticStorage'
+    STATICFILES_STORAGE = 'backend.storage_backends.StaticStorage'
 
     # # s3 private media settings
     # PRIVATE_MEDIA_LOCATION = 'private'
